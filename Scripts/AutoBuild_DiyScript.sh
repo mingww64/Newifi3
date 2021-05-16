fc1a10cc16f41bf87c38b14ea5835bea99f6c598
FeliiiciaWen <58871134+wmyfelix@users.noreply.github.com>
2021-05-16 14:13:50 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -7,7 +7,7 @@ Diy_Core() {
 	Author=
 	Default_Device=d-team_newifi-d2
 	Short_Firmware_Date=true
-	Default_IP_Address=192.168.1.1
+	Default_IP_Address="192.168.1.1"
 
 	INCLUDE_AutoUpdate=true
 	INCLUDE_AutoBuild_Tools=true
